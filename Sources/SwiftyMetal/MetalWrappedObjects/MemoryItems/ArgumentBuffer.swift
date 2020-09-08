7dab5f317b3bf3cdfb2cc428ae480a3f5bb6daa2
Michael <nuritdeveloper@gmail.com>
2020-09-08 10:06:54 -0400

    add arg buffer heap support

--- Sources/SwiftyMetal/MetalWrappedObjects/MemoryItems/ArgumentBuffer.swift
+++ Sources/SwiftyMetal/MetalWrappedObjects/MemoryItems/ArgumentBuffer.swift
@@ -10,6 +10,13 @@ public class ArgumentBuffer: MetalRepresentable {
 		
 		try self.init(encoder: encoder, configure: configure,options: options,label: label)
 	}
+    
+    /// uses the device from the encoder object to create the needed buffers
+    public convenience init<T: RawRepresentable>(function: MTLFunction,index: T,_ heap: MTLHeap, configure: (MTLArgumentEncoder)throws->(), options: MTLResourceOptions = [], label: String? = nil)throws where T.RawValue == Int {
+        let encoder = function.makeArgumentEncoder(bufferIndex: index.rawValue)
+        
+        try self.init(encoder: encoder,heap, configure: configure,options: options,label: label)
+    }
 	
 	public init(encoder: MTLArgumentEncoder, configure: (MTLArgumentEncoder)throws->(), options: MTLResourceOptions = [], label: String? = nil)throws {
 		let device = encoder.device
@@ -20,21 +27,38 @@ public class ArgumentBuffer: MetalRepresentable {
 		try configure(encoder)
 	}
 	
-	/// used for an arg buffers that is an element in an array of another arg buffer
+    public init(encoder: MTLArgumentEncoder,_ heap: MTLHeap, configure: (MTLArgumentEncoder)throws->(), options: MTLResourceOptions = [], label: String? = nil)throws {
+        self.encoder = encoder
+        mtlItem = try heap.makeBuffer(length: encoder.encodedLength, options: options).unWrapped()
+            encoder.setArgumentBuffer(mtlItem, offset: 0)
+        mtlItem.label = label
+        try configure(encoder)
+    }
+    
+	/// used for an arg buffers that is an element in an array in/of another arg buffer
 	public init(arrayElementEncoder encoder: MTLArgumentEncoder,elements: Int, configure: (MTLArgumentEncoder)throws->(), options: MTLResourceOptions = [], label: String? = nil,setArgBuf: Bool = true)throws {
 		let device = encoder.device
 		self.encoder = encoder
 		mtlItem = try device.makeBuffer(length: encoder.encodedLength * elements, options: options).unWrapped()
 		mtlItem.label = label
-//		for i in 0..<elements {
-		
 		if setArgBuf {
 			encoder.setArgumentBuffer(mtlItem, startOffset: 0, arrayElement: 0)
 		}
-//		}
 		
 		try configure(encoder)
 	}
+    
+    /// used for an arg buffers that is an element in an array in/of another arg buffer
+    public init(arrayElementEncoder encoder: MTLArgumentEncoder,_ heap: MTLHeap,elements: Int, configure: (MTLArgumentEncoder)throws->(), options: MTLResourceOptions = [], label: String? = nil,setArgBuf: Bool = true)throws {
+        self.encoder = encoder
+        mtlItem = try heap.makeBuffer(length: encoder.encodedLength * elements, options: options).unWrapped()
+        mtlItem.label = label
+        if setArgBuf {
+            encoder.setArgumentBuffer(mtlItem, startOffset: 0, arrayElement: 0)
+        }
+        
+        try configure(encoder)
+    }
 	
 	public func encode(element: Int, config: (MTLArgumentEncoder)throws->()) rethrows {
 		encoder.setArgumentBuffer(mtlItem, startOffset: 0, arrayElement: element)
