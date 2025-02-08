83e0dffac74d9a0218fb335ce6d33e6d93a02524
Michael Baron <developer.michael.baron@gmail.com>
2025-02-08 12:33:59 -0500

    update vertex descriptor initializar to public

--- Sources/SwiftyMetal/MetalWrappedObjects/VertexDescriptor.swift
+++ Sources/SwiftyMetal/MetalWrappedObjects/VertexDescriptor.swift
@@ -3,9 +3,9 @@ import Metal
 
 public class VertexDescriptor: MetalRepresentable {
 	//MARK: Init
-    typealias configurator = (VertexDescriptor)->()
+    public typealias configurator = (VertexDescriptor)->()
 	
-	init(_ configurator: configurator) {
+	public init(_ configurator: configurator) {
 		mtlItem = MTLVertexDescriptor()
 		configurator(self)
 	}
@@ -14,13 +14,13 @@ public class VertexDescriptor: MetalRepresentable {
 	
 	//MAR: Helper Methods
 	/// think of as element such as pos or uv
-	func createAttribute<VertexAttribute: RawRepresentable>(format: MTLVertexFormat,attribute: VertexAttribute) where VertexAttribute.RawValue == Int {
+	public func createAttribute<VertexAttribute: RawRepresentable>(format: MTLVertexFormat,attribute: VertexAttribute) where VertexAttribute.RawValue == Int {
 		mtlItem.attributes[attribute.rawValue].format = format
 		mtlItem.attributes[attribute.rawValue].offset = 0
 		mtlItem.attributes[attribute.rawValue].bufferIndex = attribute.rawValue
 	}
 	/// think of as buffer
-	func createLayout<VertexAttribute: RawRepresentable>(stride: Int,attribute: VertexAttribute)  where VertexAttribute.RawValue == Int {
+	public func createLayout<VertexAttribute: RawRepresentable>(stride: Int,attribute: VertexAttribute)  where VertexAttribute.RawValue == Int {
 		mtlItem.layouts[attribute.rawValue].stride = stride
 		mtlItem.layouts[attribute.rawValue].stepRate = 1
 		mtlItem.layouts[attribute.rawValue].stepFunction = .perVertex
